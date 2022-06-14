c72ccd3ced37e3f8db1d7e40db66e40f7740df9e
your Name <mohamedabdulayubm@gmail.com>
2022-06-14 11:32:31 +0530

    Added

---
+++ LineCom/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+double Length=((x2 - x1) + (y2 - y1));
+Console.WriteLine(Math.Sqrt(Length));
+
