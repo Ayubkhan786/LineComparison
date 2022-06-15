4794a43b7f230fdcb1659cd9dddc7133e6cc570a
your Name <mohamedabdulayubm@gmail.com>
2022-06-15 13:42:03 +0530

    added

---
+++ LineCom/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    public class length
+    {
+        public void findlen()
+        {
+
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double Length = ((Math.Pow(x2, 2)) - (Math.Pow(x1, 2))) + ((Math.Pow(y2, 2)) - (Math.Pow(y1, 2)));
+            Console.WriteLine(Math.Sqrt(Length));
+
+        }
+    }
+class Length
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Length of one line");
+        length find1= new length();
+        find1.findlen();
+        Console.WriteLine("Length of Second line");
+        length find2 = new length();
+        find2.findlen();
+
+
+    }
+}
--- LineCom/Program.cs
+++ LineCom/Program.cs
@@ -1,10 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Line Comparison Computation Program");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-int x1 = Convert.ToInt32(Console.ReadLine());
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-double Length = ((Math.Pow(x2, 2)) - (Math.Pow(x1, 2))) + ((Math.Pow(y2, 2)) - (Math.Pow(y1, 2)));
-Console.WriteLine(Math.Sqrt(Length));
+//int x1 = Convert.ToInt32(Console.ReadLine());
+//int x2 = Convert.ToInt32(Console.ReadLine());
+//int y1 = Convert.ToInt32(Console.ReadLine());
+//int y2 = Convert.ToInt32(Console.ReadLine());
+//double Length = ((Math.Pow(x2, 2)) - (Math.Pow(x1, 2))) + ((Math.Pow(y2, 2)) - (Math.Pow(y1, 2)));
+//Console.WriteLine(Math.Sqrt(Length));
 
